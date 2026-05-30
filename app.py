c53ce06ee762ccb7edae4621c0330fff1e4dd6b5
surya809526 <swarajdubey605@gmail.com>
2026-05-30 12:18:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,36 +3,9 @@ import os
 
 app = Flask(__name__)
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-HF_API_KEY = os.getenv("HF_API_KEY")
-
 @app.route("/")
 def home():
-
-    return f"""
-    <h2>Render Environment Test</h2>
-
-    BOT_TOKEN Found:
-    {BOT_TOKEN is not None}
-
-    <br><br>
-
-    HF_API_KEY Found:
-    {HF_API_KEY is not None}
-
-    <br><br>
-
-    BOT_TOKEN Length:
-    {len(BOT_TOKEN) if BOT_TOKEN else 0}
-
-    <br><br>
-
-    HF_API_KEY Length:
-    {len(HF_API_KEY) if HF_API_KEY else 0}
-    """
+    return str(os.getenv("TEST_VAR"))
 
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 10000))
-    )
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
