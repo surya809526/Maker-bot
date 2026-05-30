0fc4f27c1ceac669f362625650a0552fe2e62076
surya809526 <swarajdubey605@gmail.com>
2026-05-30 12:38:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,11 @@ import os
 
 app = Flask(__name__)
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
 @app.route("/")
 def home():
-    return f"""
-    TEST_VAR={os.getenv('TEST_VAR')}
-    <br>
-    BOT_TOKEN_EXISTS={os.getenv('BOT_TOKEN') is not None}
-    <br>
-    HF_API_KEY_EXISTS={os.getenv('HF_API_KEY') is not None}
-    """
+    return f"BOT TOKEN FOUND: {BOT_TOKEN is not None}"
 
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 10000))
-    )
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
