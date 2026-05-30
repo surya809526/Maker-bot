3f270f2d03511dd8db0ed3c492a7b5a0ebe30a4b
surya809526 <swarajdubey605@gmail.com>
2026-05-30 11:25:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return f"""
-BOT_TOKEN EXISTS: {"BOT_TOKEN" in os.environ}<br>
-HF_API_KEY EXISTS: {"HF_API_KEY" in os.environ}
-"""
+    return str(os.environ.get("BOT_TOKEN"))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
